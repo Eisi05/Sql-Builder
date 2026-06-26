276dc5a7f84e199b3d6c0249461d4fe7659c9da5
Eisi05 <clechi05@gmail.com>
2026-06-26 23:29:45 +0200

    update UpdateStatement

--- src/main/java/de/eisi05/sql/statements/UpdateStatement.java
+++ src/main/java/de/eisi05/sql/statements/UpdateStatement.java
@@ -1,8 +1,12 @@
 package de.eisi05.sql.statements;
 
+import de.eisi05.sql.annotations.Column;
+import de.eisi05.sql.annotations.Id;
 import de.eisi05.sql.interfaces.ExecuteUpdateStatement;
+import de.eisi05.sql.statements.where.WhereEqualStatement;
 import de.eisi05.sql.utils.OrmUtils;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class UpdateStatement extends AbstractStatement implements SetStatement.SetStatementContainer,
@@ -26,11 +30,26 @@ public class UpdateStatement extends AbstractStatement implements SetStatement.S
             return create(new UpdateStatement(table));
         }
 
-        default <T> SetStatement update(T object)
+        default <T> WhereEqualStatement update(T object)
         {
             UpdateStatement updateStatement = new UpdateStatement(OrmUtils.resolveTable(object.getClass()));
             Map<String, Object> values = OrmUtils.toColumnMap(object);
-            return create(updateStatement).set(values);
+
+            Object id = OrmUtils.extractId(object);
+
+            String idColumn = Arrays.stream(object.getClass().getDeclaredFields())
+                    .filter(f -> f.isAnnotationPresent(Id.class))
+                    .findFirst()
+                    .map(f ->
+                    {
+                        Column col = f.getAnnotation(Column.class);
+                        return (col != null && !col.name().isEmpty()) ? col.name() : f.getName();
+                    })
+                    .orElseThrow();
+
+            return create(updateStatement).set(values)
+                    .where(idColumn)
+                    .equal(OrmUtils.formatValue(id));
         }
     }
 }
