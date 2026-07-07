17ba60fd1c38571e284cb1e95aeb81a72f47ed47
Eisi05 <clechi05@gmail.com>
2026-07-07 14:53:52 +0200

    Fixed LocalDateTime

--- src/main/java/de/eisi05/sql/utils/OrmUtils.java
+++ src/main/java/de/eisi05/sql/utils/OrmUtils.java
@@ -7,6 +7,7 @@ import de.eisi05.sql.annotations.Table;
 
 import java.lang.reflect.Field;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -82,6 +83,7 @@ public class OrmUtils
             case String s -> "'" + s.replace("'", "''") + "'";
             case Enum<?> e -> "'" + e.name() + "'";
             case Timestamp t -> "'" + t + "'";
+            case LocalDateTime t -> "'" + t + "'";
             case int[] primitiveInts -> "'{" + Arrays.stream(primitiveInts)
                     .mapToObj(String::valueOf)
                     .collect(Collectors.joining(",")) + "}'";
